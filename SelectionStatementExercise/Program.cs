9303a72d930a5d9450f49089b108f2ce00648c7a
Elsa Villarreal <elsa22@att.net>
2024-05-27 21:02:31 -0500

    ex 1 complete

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace SelectionStatementExercise
+﻿using System.ComponentModel.Design;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            var faveNumber = 8;
+            Console.WriteLine("Try to guess my favorite number");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < faveNumber)
+            {
+                Console.WriteLine("Too Low, think higher!");
+            }
+            else if (userInput > faveNumber)
+            {
+                Console.WriteLine("Slow your Roll, too high!");
+            }
+            else 
+            {
+                Console.WriteLine("You got it!");
+            }
+
+        }   
     }
 }
